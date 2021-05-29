4d2be273ad8abcd2b3a72e95927281a9aa286483
Jeferson Lima <jefersonjl82@gmail.com>
2021-05-28 23:35:27 -0300

    add nonlinear class

--- src/model.cpp
+++ src/model.cpp
@@ -13,41 +13,53 @@
 
 #include "model.hpp"
 
-Linear::Linear(  
-    const Eigen::MatrixXd& A,
-    const Eigen::MatrixXd& B, 
-    const Eigen::MatrixXd& Q
-    ) : A(A), B(B), Q(Q),
-    n(A.rows()), mu_hat(n), Sigma_hat(n,n) {}
+/* @brief Linear Model */
+Linear::Linear(
+    const Eigen::MatrixXd &A,
+    const Eigen::MatrixXd &B,
+    const Eigen::MatrixXd &Q)
+    : A(A), B(B), Q(Q),
+      n(A.rows()), mu_hat(n), Sigma_hat(n, n) {}
 
-void Linear::init(const Eigen::VectorXd& mu_0, Eigen::MatrixXd& Sigma_0)
+Linear::Linear(
+    const Eigen::MatrixXd &Q, int m, int c)
+    : Q(Q), m(m), c(c), n(Q.rows()), mu_hat(n), Sigma_hat(n, n)
 {
-    mu_hat = mu_0;
-    Sigma_hat = Sigma_0;
+  A = Eigen::MatrixXd(n, n).setZero();
+  B = Eigen::MatrixXd(n, c).setZero();
+}
+
+void Linear::init(const Eigen::VectorXd &mu_0, Eigen::MatrixXd &Sigma_0)
+{
+  mu_hat = mu_0;
+  Sigma_hat = Sigma_0;
 }
 
 void Linear::init()
 {
-    mu_hat.setZero();
-    Sigma_hat.setZero();
+  mu_hat.setZero();
+  Sigma_hat.setZero();
 }
 
-void Linear::time_update(const Eigen::MatrixXd& u)
+void Linear::time_update(const Eigen::MatrixXd &u)
 {
-    mu_hat  = A * mu_hat + B * u;
-    Sigma_hat = A * Sigma_hat * A.transpose() + Q;
+  mu_hat = A * mu_hat + B * u;
+  Sigma_hat = A * Sigma_hat * A.transpose() + Q;
 }
 
-
-NonLinear::NonLinear(  
-    const Eigen::MatrixXd& Q
-    ) : Q(Q),
-    n(Q.rows()), mu_hat(n), Sigma_hat(n,
-    n) {}
-
-void NonLinear::time_update(const Eigen::MatrixXd& u)
+/* @brief Nonlinear Model */
+NonLinear::NonLinear(const Eigen::MatrixXd &Q, int m, int c)
+    : Linear(Q, m, c)
 {
 }
 
+void NonLinear::loadEq(const taylorJacobian& J)
+{
+  _J = J;
+}
 
+void NonLinear::applyTaylorJacobian()
+{
+  _J(A,B,mu_hat);
 
+}
