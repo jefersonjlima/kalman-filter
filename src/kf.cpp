537405aca575a82a46595354dd34397f1c47cc3d
Jeferson Lima <jefersonjl82@gmail.com>
2021-06-02 16:59:19 -0300

    better

---
+++ src/kf.cpp
@@ -0,0 +1,15 @@
+/* ----------------------------------------------------------------------------
+ * Copyright 2021, Jeferson Lima
+ * All Rights Reserved
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+/**
+ *  @file   ekf.h
+ *  @author Jeferson Lima
+ *  @brief  Source file for Extended Kalman Filter
+ *  @date   April 07, 2021
+ **/
+
+#include "kf.hpp"
+
