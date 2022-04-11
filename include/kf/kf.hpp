498f78475e32251de4b98c1697c71ee033026ffb
Jeferson Lima <jefersonjl82@gmail.com>
2022-04-10 21:21:27 -0300

    upgrade mesurement_update function

--- include/kf/kf.hpp
+++ include/kf/kf.hpp
@@ -18,23 +18,25 @@
 template <class T>
 class KFilter : public T
 {
-  int m, n;
+  int n_, m_, c_;
+  dFdx h_mu_;
+  Eigen::MatrixXd h_;
 
 public:
   KFilter(const Eigen::MatrixXd &A,
           const Eigen::MatrixXd &B,
           const Eigen::MatrixXd &C,
           const Eigen::MatrixXd &Q,
-          const Eigen::MatrixXd &R) : T(A, B, Q), C(C), R(R), n(A.rows())
+          const Eigen::MatrixXd &R) : T(A, B, Q), C(C), R(R), n_(A.rows())
   {
-    I.setIdentity(n, n);
+    I.setIdentity(n_, n_);
   }
 
-  KFilter(const Eigen::MatrixXd &R, const Eigen::MatrixXd &Q, int m, int c)
-      : T(Q, m, c), R(R), n(Q.rows()), m(m)
+  KFilter(const Eigen::MatrixXd &R, const Eigen::MatrixXd &Q, int n, int m, int c)
+      : T(Q, n, m, c), R(R), n_(n), m_(m), c_(c)
   {
-    I.setIdentity(n, n);
-    C = Eigen::MatrixXd(m, n).setZero();
+    I.setIdentity(n_, n_);
+    C = Eigen::MatrixXd(m_, n_).setZero();
   }
 
   ~KFilter(){};
@@ -42,10 +44,25 @@ public:
   void measurement_update(const Eigen::MatrixXd &z)
   {
     auto K = this->Sigma_hat * C.transpose() * (C * this->Sigma_hat * C.transpose() + R).inverse();
-    this->mu_hat = this->mu_hat + K * (z - (C * this->mu_hat));
+    if (this->is_linear)
+    {
+    	this->mu_hat = this->mu_hat + K * (z - (C * this->mu_hat));
+    }
+    else
+    	this->mu_hat = this->mu_hat + K * (z - h_);
+
     this->Sigma_hat = (I - K * C) * this->Sigma_hat;
   }
 
+  void loadSensorEqs(const dFdx& h)
+  {
+    h_mu_ = h;
+  }
+
+  void applySensorJacobian()
+  {
+    std::tie(C, h_) = h_mu_(this->mu_hat,n_, m_, c_);
+  }
 
   Eigen::MatrixXd R, C, I;
 
